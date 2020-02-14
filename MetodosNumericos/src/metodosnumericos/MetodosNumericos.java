760f6c93435177893bbe496e9fbd5e5e798f66dc
jcestrella49 <34067766+jcestrella49@users.noreply.github.com>
2020-02-13 20:19:21 -0600

    Add files via upload

---
+++ MetodosNumericos/src/metodosnumericos/MetodosNumericos.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metodosnumericos;
+
+/**
+ *
+ * @author Juan Carlos Estrella
+ */
+public class MetodosNumericos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        frmPrincipal ventana = new frmPrincipal();
+        ventana.setVisible(true);
+    }
+    
+}
