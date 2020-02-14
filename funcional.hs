035879c78d07d246b9c4bf62dae57a61f3327abd
jcestrella49 <34067766+jcestrella49@users.noreply.github.com>
2020-02-13 22:59:51 -0600

    Update funcional.hs

--- funcional.hs
+++ funcional.hs
@@ -5,3 +5,12 @@ division x y = x `div` y
 
 cuadrado :: Int -> Int
 cuadrado x = x * x
+
+qsort [] = []
+qsort (x:xs) = qsort menores ++ [x] ++ qsort mayores
+  where menores = filter (<x) xs
+        mayores = filter (>=x) xs
+
+esMayor x y = if (x > y)
+    then "es mayor"
+    else "no es mayor"
